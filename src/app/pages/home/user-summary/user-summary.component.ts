b25f9a8a24795d13cbe1c009583a0ee54460097f
Ahmad Muahsien <ahmuhaisen03@gmail.com>
2024-09-25 14:09:45 +0300

    Fix view profile Link

--- src/app/pages/home/user-summary/user-summary.component.ts
+++ src/app/pages/home/user-summary/user-summary.component.ts
@@ -1,3 +1,4 @@
+import { RouterLink } from '@angular/router';
 import { Component, inject, OnInit } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faAddressBook, faSquareUpRight, faUserLarge, faUsers } from '@fortawesome/free-solid-svg-icons';
@@ -8,7 +9,10 @@ import { UserService } from '../../../core/services/user.service';
 @Component({
   selector: 'app-user-summary',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [
+    RouterLink,
+    FontAwesomeModule
+  ],
   templateUrl: './user-summary.component.html',
   styleUrl: './user-summary.component.css'
 })
